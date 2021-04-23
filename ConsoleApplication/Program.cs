ba444af05a880300b542571c5c5e1740faf78098
Greg Witt <36649901+GoodGuyGregory@users.noreply.github.com>
2021-04-23 10:18:50 -0400

    Add files via upload

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+     class Program
+     {
+          static void Main(string[] args) { 
+               
+               if (DateTime.Now.Hour < 12) {
+                    Console.WriteLine("Good Morning, from a console application");
+               }
+               else
+               {
+                    Console.WriteLine("Good Day, from a console application");
+               }
+               
+               Console.WriteLine("The time is currently: {0}", DateTime.Now);
+               Console.WriteLine("Plese tell me your name");
+               string name = Console.ReadLine();
+               Console.WriteLine($"Hello {name}!");
+          }
+     }
+}
