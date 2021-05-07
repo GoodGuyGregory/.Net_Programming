816e6d44f826a74d9673775b7240aa9597b2f0a6
GoodGuyGregory <greg.witt625@gmail.com>
2021-05-07 16:46:22 -0400

    adds Collections project

---
+++ C#_.NET/Collections/Lists/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Create and istantiate a list
+            List<String> customers = new List<string>();
+
+            customers.Add("Kim");
+            customers.Add("John");
+            customers.Add("Tim");
+
+            //  Output the size of the list
+            System.Console.WriteLine($"Customers contains: {customers.Count} items");
+
+            foreach (var customer in customers)
+            {
+                System.Console.WriteLine(customer);
+            }
+
+            Console.WriteLine($"{customers[1]} is a great customer");
+        }
+    }
+}
