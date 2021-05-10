393074e5491f4e65ee69241fb5a73d7cd8b952c9
GoodGuyGregory <greg.witt625@gmail.com>
2021-05-10 11:31:50 -0400

    adds new resources for collections

---
+++ C#_.NET/Collections/Array_Lists/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace Array_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // arraylists use boxing because it is just a collection type that 
+            //  can hold all type defs
+            ArrayList list = new ArrayList();
+
+            list.Add("udefined string type");
+
+            //  this is BOXING: it uses a generic object to store a value
+            int i = 123;
+
+            object o = i;
+
+            // boxing :
+            //  now it is recast to its original data type
+            i = (int)o;
+
+            // ACCESSING ELEMENTS:
+            String s = (String)list[0];
+
+            Console.WriteLine($"{s} is inside of the Arraylist");
+
+
+        }
+    }
+}
---
+++ C#_.NET/Collections/Dictionaries/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<String, String> config = new Dictionary<string, string>();
+
+            config.Add("resolution", "1920X1080");
+            config.Add("title", "MyWebsite");
+
+            Console.WriteLine($"Title: {config["title"]}");
+            Console.WriteLine($"Resolution: {config["resolution"]}");
+
+            // iterates through the dictionary:
+            foreach (var key in config)
+            {
+                Console.WriteLine($"Key: {key.Key}, Value: {key.Value}");
+            }
+        }
+    }
+}
