10a2a0a0ad500271a42593057b146d8a3e7ed1c7
GoodGuyGregory <greg.witt625@gmail.com>
2021-05-10 13:28:00 -0400

    adds async

---
+++ C#_.NET/Async-Programming/LearningAsync/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace LearningAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Downloading File...");
+            Download();
+            //  because of Task.Run this allows for us to complete the Readline command
+            Console.ReadLine();
+        }
+
+        static void Download()
+        {
+            //  allows for the skipping of this method if it takes a longer amount of time
+            Task.Run(() =>
+            {
+
+                //  Simulate downloading the file
+                Thread.Sleep(5000);
+                Console.WriteLine("Download Complete");
+
+            });
+
+        }
+
+    }
+}
