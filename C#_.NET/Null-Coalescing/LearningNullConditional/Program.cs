262f28f45905410d9350ca5afe765d4e03acf061
GoodGuyGregory <greg.witt625@gmail.com>
2021-05-10 13:52:35 -0400

    adds null project

---
+++ C#_.NET/Null-Coalescing/LearningNullConditional/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LearningNullConditional
+{
+
+    class Person
+    {
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person() { Name = "Tim" }; // what if this was NULL?
+            Console.WriteLine(person.Name);
+
+            //  Basic Checking for NULL without the NULL operator
+            Person nullPerson = null;
+            if (nullPerson != null)
+            {
+                Console.WriteLine(nullPerson.Name);
+            }
+            else
+            {
+                Console.WriteLine("Person's name is N/A");
+            }
+
+            // Using the NULL OPERATOR:
+            //  This can be configured on properties of any element 
+            Console.WriteLine(nullPerson?.Name);
+
+            Person personWithoutName = new Person();
+            //  This will use the coalesce elements to allow for the default to be replaced with "Not Set yet"
+            Console.WriteLine(personWithoutName?.Name ?? "Not Set yet");
+
+
+        }
+    }
+}
---
+++ C#_.NET/Null-Coalescing/null-ing-around/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace null_ing_around
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // with int? it allows the value to  be null
+            int? s = null; //"hello"
+
+            //  if the value is infact null coalese it into being 2
+            int s2 = s ?? 2;
+
+            Console.WriteLine(s2);
+        }
+    }
+}
